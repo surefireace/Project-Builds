84c216f8013e31711e9a596ba0bd0c1e338ec060
Donovan Colen <surefireace@hotmail.com>
2019-10-20 15:48:41 -0700

    initial commit
    added booty pacman and mall sim builds

---
+++ Booty Quest/README.txt
@@ -0,0 +1,7 @@
+How To Play:
+WASD movement
+LMB fire left cannons
+RMB fire right cannons
+Left Shift + mouse movement camera controls
+C reset camera
+Dock at the Island with the skull to Upgrade and Repair the ship.
---
+++ Pac-Man Recreation/x86/Assets/Map/Pac-Man_map_source.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Pac-Man map source" tilewidth="16" tileheight="16" spacing="0" margin="0" tilecount="48" columns="8">
+ <image source="Pac-Man_map_source.png" width="128" height="96"/>
+</tileset>
